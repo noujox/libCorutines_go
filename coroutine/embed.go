f29a84ee4915d971995ce010c58e0cb465d2c102
Noujox <darkosa15@gmail.com>
2019-10-18 09:00:22 -0300

    Subida de libreria de Freezerburn

---
+++ coroutine/embed.go
@@ -0,0 +1,165 @@
+package coroutine
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+// The base struct that has all the functions necessary to operate on a coroutine. It is designed to be used in one of
+// two ways:
+// - As the argument to a func passed to one of the Start functions. It can then be used as a proxy for the
+//   coroutine function. e.g.: `msg := e.Recv()`
+// - Embedded directly into another struct, so that the struct can call the coroutine functions as if they were its
+//   own. e.g.: `type A struct { coroutine.Embeddable }` The struct that embeds the coroutine struct can then
+//   implement the Start method on the Starter interface and an instance of it can be passed directly to one of the
+//   Start functions. Embeddable MUST be embedded as a non-pointer, and the struct embedding it MUST be used as a
+//   pointer.
+type Embeddable struct {
+	id           uint64
+	name         string
+	waitTimer    *time.Timer
+	receiver     chan bool
+	receiveTimer *time.Timer
+	mailbox      []interface{}
+	mailboxLock  sync.Mutex
+	running      bool
+}
+
+// Pauses execution of this coroutine for the given duration to allow other coroutines to run.
+//
+// If this coroutine has been stopped by external code using the Ref returned by all Start functions, then it will
+// immediately stop, and no further code outside of deferred functions will be executed in this coroutine.
+func (e *Embeddable) Pause(d time.Duration) {
+	if !e.running {
+		// Every coroutine is wrapped in a function that recovers from a panic, so this is guaranteed to immediately
+		// stop execution of the coroutine completely without stopping the rest of the program.
+		panic(Stop{})
+	}
+
+	resetTimer(e.waitTimer, d)
+	<-e.waitTimer.C
+
+	// Since there's a period of time that this is doing nothing, there's a chance that external code could stop
+	// this coroutine while it's paused. So we check that before returning control to the coroutine.
+	if !e.running {
+		panic(Stop{})
+	}
+}
+
+// Instead of constructing a new Timer object, which can be expensive if done frequently, we reset an existing one
+// to a given duration immediately before using it.
+func resetTimer(t *time.Timer, d time.Duration) {
+	if !t.Stop() {
+		// I've seen cases where Stop returns false, but there isn't an item in the channel, causing the receive to
+		// hang forever.
+		select {
+		case <-t.C:
+		default:
+		}
+	}
+	t.Reset(d)
+}
+
+// Checks the mailbox for any sent messages. If none are in the mailbox, this function will halt the coroutine until
+// one gets sent.
+//
+// If this coroutine has been stopped by external code using the Ref returned by all Start functions, then it will
+// immediately stop, and no further code outside of deferred functions will be executed in this coroutine.
+func (e *Embeddable) Recv() interface{} {
+	if !e.running {
+		panic(Stop{})
+	}
+
+	e.mailboxLock.Lock()
+	if len(e.mailbox) == 0 {
+		e.mailboxLock.Unlock()
+		<-e.receiver
+
+		if !e.running {
+			panic(Stop{})
+		}
+
+		e.mailboxLock.Lock()
+	}
+
+	r := e.mailbox[0]
+	e.mailbox = e.mailbox[1:]
+	e.mailboxLock.Unlock()
+	return r
+}
+
+// Checks if the mailbox contains anything. If it does, that value and true are returned. If it doesn't, the
+// coroutine will pause for up to duration time. If a value is put into the mailbox within that time, that value and
+// true are returned. If nothing was put into the mailbox during that time, nil and false are returned.
+//
+// If this coroutine has been stopped by external code using the Ref returned by all Start functions, then it will
+// immediately stop, and no further code outside of deferred functions will be executed in this coroutine.
+func (e *Embeddable) RecvFor(d time.Duration) (interface{}, bool) {
+	if !e.running {
+		panic(Stop{})
+	}
+
+	if d <= 0 {
+		return e.RecvImmediate()
+	}
+
+	e.mailboxLock.Lock()
+	if len(e.mailbox) == 0 {
+		e.mailboxLock.Unlock()
+
+		resetTimer(e.receiveTimer, d)
+		select {
+		case <-e.receiver:
+		case <-e.receiveTimer.C:
+		}
+
+		if !e.running {
+			panic(Stop{})
+		}
+
+		e.mailboxLock.Lock()
+	}
+
+	if len(e.mailbox) == 0 {
+		e.mailboxLock.Unlock()
+		return nil, false
+	}
+
+	r := e.mailbox[0]
+	e.mailbox = e.mailbox[1:]
+	e.mailboxLock.Unlock()
+	return r, true
+}
+
+// Checks if the mailbox contains anything. If it doesn't, nil and false are returned. If something is in the mailbox,
+// that value and true are returned. The found value is removed from the mailbox.
+//
+// If this coroutine has been stopped by external code using the Ref returned by all Start functions, then it will
+// immediately stop, and no further code outside of deferred functions will be executed in this coroutine.
+func (e *Embeddable) RecvImmediate() (interface{}, bool) {
+	if !e.running {
+		panic(Stop{})
+	}
+
+	e.mailboxLock.Lock()
+	defer e.mailboxLock.Unlock()
+	if len(e.mailbox) == 0 {
+		return nil, false
+	}
+
+	r := e.mailbox[0]
+	e.mailbox = e.mailbox[1:]
+	return r, true
+}
+
+// Immediately stop this coroutine. No more code in the coroutine will run, so be sure to do any cleanup work before
+// calling this function, or have a deferred function that will do your cleanup work.
+func (e *Embeddable) Stop() {
+	if !e.running {
+		log.Printf("Coroutine [%v / %s] attempted to stop itself when it isn't running, possible bug found.",
+			e.id, e.name)
+	}
+	e.running = false
+	panic(Stop{})
+}
